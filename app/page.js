9601f69ba77e844e8ba9894c97c794649995b541
qveys <contact@quentinveys.be>
2025-09-26 00:48:39 +0200

    ✨ feat(home): add quote section and remove blog component

--- app/page.js
+++ app/page.js
@@ -1,9 +1,7 @@
-import { personalData } from "@/utils/data/personal-data";
-import AboutSection from "./components/homepage/about";
-import Blog from "./components/homepage/blog";
+import {personalData} from "@/utils/data/personal-data";
+import QuoteSection from "./components/homepage/quote";
 import ContactSection from "./components/homepage/contact";
 import Education from "./components/homepage/education";
-import Experience from "./components/homepage/experience";
 import HeroSection from "./components/homepage/hero-section";
 import Projects from "./components/homepage/projects";
 import Skills from "./components/homepage/skills";
@@ -17,24 +15,22 @@ async function getData() {
 
   const data = await res.json();
 
-  const filtered = data.filter((item) => item?.cover_image).sort(() => Math.random() - 0.5);
-
-  return filtered;
-};
+  return data.filter((item) => item?.cover_image).sort(() => Math.random() - 0.5);
+}
 
 export default async function Home() {
-  const blogs = await getData();
+  //const blogs = await getData();
 
   return (
-    <div suppressHydrationWarning >
-      <HeroSection />
-      <AboutSection />
-      <Experience />
-      <Skills />
-      <Projects />
-      <Education />
-      <Blog blogs={blogs} />
-      <ContactSection />
+    <div suppressHydrationWarning>
+      <HeroSection/>
+      <QuoteSection/>
+      <Education/>
+      <Skills/>
+      <Projects/>
+      {/**<Experience/>
+       <Blog blogs={blogs}/>**/}
+      <ContactSection/>
     </div>
   )
 };
