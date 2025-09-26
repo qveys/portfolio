b4177d9557a262ad9f72062eaad5939a8b86f2aa
qveys <contact@quentinveys.be>
2025-09-26 02:48:13 +0200

    📈 Update Navbar component with logo and Image imports.

--- app/components/navbar.jsx
+++ app/components/navbar.jsx
@@ -1,16 +1,19 @@
 // @flow strict
 import Link from "next/link";
-
+import Image from "next/image";
 
 function Navbar() {
   return (
     <nav className="bg-transparent">
       <div className="flex items-center justify-between py-5">
         <div className="flex flex-shrink-0 items-center">
-          <Link
-            href="/"
-            className=" text-[#16f2b3] text-3xl font-bold">
-            ABU SAID
+          <Link href="/">
+            <Image
+              src="/logo.svg"
+              alt="Logo - Quentin Veys"
+              width={50}
+              height={50}
+            />
           </Link>
         </div>
 
